a7cd49c11354311a4291a64688afe0925f976985
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-09-06 11:16:21 -0600

    [jyos][Agregando nuevos cambios]

---
+++ GrupoNRJ.Modelos.GestionCafe/ProductoRespuesta.cs
@@ -0,0 +1,15 @@
+﻿namespace GrupoNRJ.Modelos.GestionCafe
+{
+    public class ProductoRespuesta
+    {
+        public ProductoRespuesta()
+        {
+            Nombre = string.Empty;
+            Grano = string.Empty;
+            Cantidad = 0;
+        }
+        public string Nombre { get; set; }
+        public string Grano { get; set; }
+        public double Cantidad { get; set; }
+    }
+}
---
+++ GrupoNRJ.Modelos.GestionCafe/RespuestaBase.cs
@@ -0,0 +1,16 @@
+﻿namespace GrupoNRJ.Modelos.GestionCafe
+{
+    public class RespuestaBase<T>
+    {
+        public RespuestaBase()
+        {
+            Codigo = 0;
+            Mensaje = string.Empty;
+            Datos = default;
+        }
+
+        public int Codigo { get; set; }
+        public string Mensaje { get; set; }
+        public T? Datos { get; set; }
+    }
+}
