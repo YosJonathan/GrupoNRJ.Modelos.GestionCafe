1572e60eb9dba3ea154b790a6ca942a15c099683
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-09-12 17:42:10 -0600

    [jyos][Guardando cambios de gestion de inventario, cafe]

---
+++ GrupoNRJ.Modelos.GestionCafe/Respuestas/GranosRespuesta.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="GranosRespuesta.cs" company="GrupoAnalisis">
+// Copyright (c) GrupoAnalisis. All rights reserved.
+// </copyright>
+
+namespace GrupoNRJ.Modelos.GestionCafe.Respuestas
+{
+    public class GranosRespuesta
+    {
+        public GranosRespuesta()
+        {
+            this.IdGranos = 0;
+            this.Nombre = string.Empty;
+        }
+
+        public int IdGranos { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
---
+++ GrupoNRJ.Modelos.GestionCafe/Respuestas/ObtenerAlertasRespuesta.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="ObtenerAlertasRespuesta.cs" company="GrupoAnalisis">
+// Copyright (c) GrupoAnalisis. All rights reserved.
+// </copyright>
+
+namespace GrupoNRJ.Modelos.GestionCafe.Respuestas
+{
+    public class ObtenerAlertasRespuesta
+    {
+        public ObtenerAlertasRespuesta()
+        {
+            this.IdProducto = 0;
+            this.NombreProducto = string.Empty;
+            this.AlertaStock = false;
+            this.CantidadMinima = 0;
+            this.Existencias = 0;
+        }
+
+        public int IdProducto { get; set; }
+
+        public string NombreProducto { get; set; }
+
+        public bool AlertaStock { get; set; }
+
+        public double CantidadMinima { get; set; }
+
+        public double Existencias { get; set; }
+    }
+}
