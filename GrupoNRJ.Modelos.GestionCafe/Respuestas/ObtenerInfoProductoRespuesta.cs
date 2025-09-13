a1fe4d79d2a00c70246b9baa67db03bcaf8e9672
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-09-12 22:49:48 -0600

    [jyos][Modelos para modificar]

---
+++ GrupoNRJ.Modelos.GestionCafe/Respuestas/ObtenerInfoProductoRespuesta.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="ObtenerInfoProductoRespuesta.cs" company="GrupoAnalisis">
+// Copyright (c) GrupoAnalisis. All rights reserved.
+// </copyright>
+
+namespace GrupoNRJ.Modelos.GestionCafe.Respuestas
+{
+    public class ObtenerInfoProductoRespuesta
+    {
+        public ObtenerInfoProductoRespuesta()
+        {
+            this.IdProducto = 0;
+            this.Nombre = string.Empty;
+            this.GranoId = 0;
+            this.ValorMinimo = 0;
+        }
+
+        public int IdProducto { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int GranoId { get; set; }
+
+        public double ValorMinimo { get; set; }
+    }
+}
---
+++ GrupoNRJ.Modelos.GestionCafe/Solicitudes/ObtenerInfoProductoSolicitud.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="ObtenerInfoProductoSolicitud.cs" company="GrupoAnalisis">
+// Copyright (c) GrupoAnalisis. All rights reserved.
+// </copyright>
+
+namespace GrupoNRJ.Modelos.GestionCafe.Solicitudes
+{
+    public class ObtenerInfoProductoSolicitud
+    {
+        public ObtenerInfoProductoSolicitud()
+        {
+            this.IdProducto = 0;
+        }
+
+        public int IdProducto { get; set; }
+    }
+}
