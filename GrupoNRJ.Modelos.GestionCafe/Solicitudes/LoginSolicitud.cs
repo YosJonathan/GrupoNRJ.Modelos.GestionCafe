0bcf407a61ff8aa712d33b06d3276408d02fbff7
Jonathan Estuardo Yos Escobar <78168255+YosJonathan@users.noreply.github.com>
2025-10-24 22:34:09 -0600

    [jyos][Autenticación jwt token modelo]

---
+++ GrupoNRJ.Modelos.GestionCafe/Solicitudes/LoginSolicitud.cs
@@ -0,0 +1,15 @@
+﻿namespace GrupoNRJ.Modelos.GestionCafe.Solicitudes
+{
+    public class LoginSolicitud
+    {
+        public LoginSolicitud()
+        {
+            this.Username = string.Empty;
+            this.Password = string.Empty;
+        }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
